beba9b13265240c8a5ebfa366aa7ea68a5f7c453
Brendan Chukwuemeka <chukwuemekachukwudi9@gmail.com>
2023-09-03 19:45:50 +0300

    Refactor homepage to new screen

---
+++ lib/homepage.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:ohm_guru/components/title_card.dart';
+import 'package:ohm_guru/components/calculation_page.dart';
+import 'package:ohm_guru/constants/band_constants.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int bandCount = 4;
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: bandTypesCount,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.secondary,
+          title: Text(widget.title),
+        ),
+        body: ListView(
+          children: [
+            TitleCard(
+              bandCount: bandCount,
+            ),
+            const Divider(
+              indent: 90.0,
+              endIndent: 240.0,
+            ),
+            Container(
+              decoration: BoxDecoration(
+                border: Border.all(width: 2, color: Colors.white54),
+              ),
+              height: 800.0,
+              margin: const EdgeInsets.fromLTRB(90, 40, 240, 10),
+              child: Column(
+                children: [
+                  const Expanded(
+                    flex: 2,
+                    child: SizedBox(),
+                  ),
+                  TabBar(
+                    tabs: bandTypesTabs,
+                    indicatorColor: Colors.red,
+                    onTap: (index) {
+                      switch (index) {
+                        case 0:
+                          bandCount = 4;
+                        case 1:
+                          bandCount = 5;
+                        case 2:
+                          bandCount = 6;
+                      }
+                      setState(() {});
+                    },
+                  ),
+                  const Expanded(
+                    flex: 9,
+                    child: TabBarView(
+                      children: [
+                        CalculationPage(
+                          tempString: "Resistor Parameters 1",
+                        ),
+                        CalculationPage(
+                          tempString: "Resistor Parameters 2",
+                        ),
+                        CalculationPage(
+                          tempString: "Resistor Parameters 3",
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:ohm_guru/components/title_card.dart';
-import 'package:ohm_guru/components/calculation_page.dart';
-import 'package:ohm_guru/constants/band_constants.dart';
+import 'package:ohm_guru/homepage.dart';
 
 void main() => runApp(const MyApp());
 
@@ -18,90 +16,7 @@ class MyApp extends StatelessWidget {
           secondary: Color(0xff0058c5),
         ),
       ),
-      home: const MyHomePage(title: 'Resistor Color Code Calculator'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int bandCount = 4;
-
-  @override
-  Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: bandTypesCount,
-      child: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Theme.of(context).colorScheme.secondary,
-          title: Text(widget.title),
-        ),
-        body: ListView(
-          children: [
-            TitleCard(
-              bandCount: bandCount,
-            ),
-            const Divider(
-              indent: 90.0,
-              endIndent: 240.0,
-            ),
-            Container(
-              decoration: BoxDecoration(
-                border: Border.all(width: 2, color: Colors.white54),
-              ),
-              height: 800.0,
-              margin: const EdgeInsets.fromLTRB(90, 40, 240, 10),
-              child: Column(
-                children: [
-                  const Expanded(
-                    flex: 2,
-                    child: SizedBox(),
-                  ),
-                  TabBar(
-                    tabs: bandTypesTabs,
-                    indicatorColor: Colors.red,
-                    onTap: (index) {
-                      switch (index) {
-                        case 0:
-                          bandCount = 4;
-                        case 1:
-                          bandCount = 5;
-                        case 2:
-                          bandCount = 6;
-                      }
-                      setState(() {});
-                    },
-                  ),
-                  const Expanded(
-                    flex: 9,
-                    child: TabBarView(
-                      children: [
-                        CalculationPage(
-                          tempString: "Resistor Parameters 1",
-                        ),
-                        CalculationPage(
-                          tempString: "Resistor Parameters 2",
-                        ),
-                        CalculationPage(
-                          tempString: "Resistor Parameters 3",
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
+      home: const HomePage(title: 'Resistor Color Code Calculator'),
     );
   }
 }
