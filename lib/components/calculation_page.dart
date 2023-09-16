dcebc421fb0749336eab5f6073e37b4097901eb4
Brendan Chukwuemeka <chukwuemekachukwudi9@gmail.com>
2023-09-16 20:05:52 +0300

    Completed form of dynamic band
    Completed form of dynamic band to be further expanded upon

--- lib/components/calculation_page.dart
+++ lib/components/calculation_page.dart
@@ -146,40 +146,74 @@ class _CalculationPageState extends State<CalculationPage> {
         Expanded(
           child: Padding(
             padding: const EdgeInsets.all(20.0),
-            child: LayoutBuilder(
-                builder: (BuildContext context, BoxConstraints constraints) {
-              return Column(
-                children: [
-                  const Expanded(
-                    child: Padding(
-                      padding: EdgeInsets.all(15.0),
-                      child: Align(
-                        alignment: Alignment.topLeft,
-                        child: Text(
-                          "Output",
+            child: Column(
+              children: [
+                const Expanded(
+                  child: Padding(
+                    padding: EdgeInsets.all(15.0),
+                    child: Align(
+                      alignment: Alignment.topLeft,
+                      child: Text(
+                        "Output",
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  flex: 4,
+                  child: Stack(
+                    children: [
+                      const Align(
+                        alignment: Alignment.topCenter,
+                        child: Image(
+                          image: AssetImage("assets/resistor.png"),
                         ),
                       ),
-                    ),
-                  ),
-                  Expanded(
-                    flex: 4,
-                    child: SizedBox(
-                      width: constraints.maxWidth,
-                      child: const Image(
-                        image: AssetImage("assets/resistor.png"),
+
+                      //TODO: Refactor Alignment widget [dynamicColorBand] so its its own widget, properties would be alignment, color and container height
+                      Align(
+                        alignment: const Alignment(0, -0.7),
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Container(
+                              height: 200.0,
+                              width: 13.0,
+                              decoration: BoxDecoration(
+                                gradient: LinearGradient(
+                                  begin: Alignment.topCenter,
+                                  end: Alignment.bottomCenter,
+                                  colors: <Color>[
+                                    Colors.pink,
+                                    Colors.pink.shade200,
+                                    Colors.pink.shade50,
+                                  ],
+                                ),
+                              ),
+                            ),
+                            Container(
+                              height: 30,
+                              width: 50,
+                              decoration: BoxDecoration(
+                                color: Colors.pink,
+                                borderRadius: BorderRadius.circular(5.0),
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
-                    ),
+                    ],
                   ),
-                  const Expanded(
-                    flex: 2,
-                    child: Text(
-                      "Resistor Value:",
-                      style: TextStyle(fontSize: 40.0),
-                    ),
+                ),
+                const Expanded(
+                  flex: 2,
+                  child: Text(
+                    "Resistor Value:",
+                    style: TextStyle(fontSize: 40.0),
                   ),
-                ],
-              );
-            }),
+                ),
+              ],
+            ),
           ),
         ),
       ],
